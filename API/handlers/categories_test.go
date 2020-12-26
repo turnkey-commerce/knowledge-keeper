6bd2d0d5028ad9b8b40b68e8c4a2944011a3bc1e
James Culbertson <info@turnkey-commerce.com>
2020-12-26 10:21:42 -0600

    Add Test for categories by name and id.

--- API/handlers/categories_test.go
+++ API/handlers/categories_test.go
@@ -129,6 +129,65 @@ func TestCanViewRecentCategories(t *testing.T) {
 	assert.Equal(t, category1Name, categories[0].Name)
 }
 
+// TestCanGetCategoryByNameOrID tests that a category can be retrieved by its name or CategoryID.
+func TestCanGetCategoryByNameOrID(t *testing.T) {
+	// Setup
+	db, err := sql.Open("knowledge", "identifier")
+	defer db.Close()
+	checkError(err)
+
+	rec := httptest.NewRecorder()
+	e := echo.New()
+	h := NewHandler(db, "secret")
+
+	// First register the user.
+	assert.NoError(t, registerUser(db, e, rec, h, user1JSON))
+
+	// Login the user
+	rec = httptest.NewRecorder()
+	assert.NoError(t, loginUser1(db, e, rec, h, user1LoginJSON))
+	token := getLoginToken(rec)
+
+	// Create the category
+	rec = httptest.NewRecorder()
+	assert.NoError(t, createCategory(db, e, rec, h, token))
+	// Get the ID of the new Category
+	var category models.Category
+	json.Unmarshal([]byte(rec.Body.String()), &category)
+	id := category.CategoryID
+
+	// Setup to get the category by name.
+	rec = httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/categories/name/{name}", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	c := e.NewContext(req, rec)
+	c.SetPath("/categories/name/:name")
+	c.SetParamNames("name")
+	c.SetParamValues(category1Name)
+	// Assert
+	assert.NoError(t, h.GetCategoryByName(c))
+	var categoryByName models.Category
+	json.Unmarshal([]byte(rec.Body.String()), &categoryByName)
+	assert.Equal(t, category1Name, categoryByName.Name)
+
+	// Setup to get the category by id.
+	rec = httptest.NewRecorder()
+	req = httptest.NewRequest(http.MethodGet, "/categories/{id}", nil)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	c = e.NewContext(req, rec)
+	c.SetPath("/categories/:id")
+	c.SetParamNames("id")
+	c.SetParamValues(fmt.Sprintf("%d", id))
+	// Assert
+	assert.NoError(t, h.GetCategoryByCategoryID(c))
+	var categoryById models.Category
+	json.Unmarshal([]byte(rec.Body.String()), &categoryById)
+	assert.Equal(t, id, categoryById.CategoryID)
+	assert.Equal(t, category1Name, categoryById.Name)
+}
+
+// Private functions
+
 func createCategory(db *sql.DB, e *echo.Echo, rec *httptest.ResponseRecorder, h *Handler, token string) error {
 	req := httptest.NewRequest(http.MethodPost, "/categories",
 		strings.NewReader(getCategory1JSON(category1Name)))
